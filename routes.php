fdb8b430d3b3956d4d6e28b8c448cdac555cc46c
LeoZhangYYDS <zhanglingxiao1991@gmail.com>
2024-07-25 11:27:48 +1000

    add-update function, all CRUD finish

--- routes.php
+++ routes.php
@@ -1,9 +1,11 @@
 <?php
-
-
 $router->get('/', 'HomeController@index');
 $router->get('/listings', 'ListingController@index');
 $router->get('/listings/create', 'ListingController@create');
 $router->get('/listings/{id}', 'ListingController@show');
+$router->get('/listings/edit/{id}','ListingController@edit');
+
+
 $router->post('/listings', 'ListingController@store');
 $router->delete('/listings/{id}','ListingController@destroy');
+$router->put('/listings/{id}','ListingController@update');
